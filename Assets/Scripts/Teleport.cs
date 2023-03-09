5926066e317447e8fc94fbc1c54c7b7ec4da4249
matthewrpolycarp <62485115+matthewrpolycarp@users.noreply.github.com>
2023-03-09 11:21:46 +0100

    scripty doo

---
+++ Assets/Scripts/Teleport.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleport : MonoBehaviour
+{
+    public GameObject parental;
+    public Vector3 telePoint;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Barrier")
+        {
+            parental.transform.position = telePoint
+        }
+    }
+}
